a74bb00f551fc4039ec5dea67691a63e457b17e4
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-06 22:38:12 -0400

    Clases abstractas

---
+++ src/12-abstract.ts
@@ -0,0 +1,8 @@
+import { Animal, Dog } from './09-Acceso-protegido';
+
+// const animal = new Animal('elite');
+// animal.greeting();
+
+const cheils = new Dog('cheils', 'gerardo');
+cheils.greeting();
+cheils.woof(2);
