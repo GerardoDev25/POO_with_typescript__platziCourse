e833d7d2c9fd4b64e9dad26a91619a856e21fbea
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-07 23:18:45 -0400

    Proyecto: migración de funciones a clases

--- src/app/01-main.ts
+++ src/app/01-main.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Product } from '../models/products.model';
+import { Product } from './models/products.model';
 
 (async () => {
   async function getProducts() {
