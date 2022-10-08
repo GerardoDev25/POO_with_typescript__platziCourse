827806c6c5f7a1eb6ea479c177689456e46b0558
Gerardo Miranda <gerardodev25@gmail.com>
2022-10-08 00:07:15 -0400

    Consumiendo ProductHttpService

---
+++ src/app/03-main.ts
@@ -0,0 +1,21 @@
+import { ProductHttpService } from './services/product.http.service';
+
+(async () => {
+  const productsSevice = new ProductHttpService();
+
+  try {
+    console.log('-'.repeat(20));
+    const products = await productsSevice.getAll();
+    // console.log(products.map((e) => e));
+
+    const productId = products[0].id;
+    const productUpdate = await productsSevice.update(productId, {
+      price: 1000,
+      title: 'title product change',
+      description: 'product description changed',
+    });
+    console.log(productUpdate);
+  } catch (error) {
+    console.log(error);
+  }
+})();
